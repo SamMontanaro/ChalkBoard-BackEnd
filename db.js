3cad156ae48fbbab0edaceb24a4b753357250789
Sam Montanaro <sammontanaro12@gmail.com>
2022-02-22 05:38:31 -0500

    Connecting to Heroku

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Npalermo211$",
-    host: "localhost",
-    database: "chalkboard",
+    user: "qjddgyobscjkrv",
+    password: "5fd261038e49cce27765f27aa354f303e72541c80ea818320ec7d2014f6a9750$",
+    host: "ec2-52-23-63-127.compute-1.amazonaws.com",
+    database: "de5im3r7gptrg1",
     port: 5432
 });
 
